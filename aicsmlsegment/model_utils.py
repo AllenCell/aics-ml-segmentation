fix normalization in config for caax, add metrics

aicsmlsegment/model_utils.py
@@ -3,10 +3,11 @@ import torch
 from monai.inferers import sliding_window_inference
 
 
-def apply_on_image(model, input_img, args):
+def apply_on_image(model, input_img, args, squeeze, to_numpy):
     """
     Perform inference on an input img through a model with or without runtime augmentation.
     If runtime augmentation is selected, perform inference on flipped images and average results.
+    returns: 5d np.array NCZYX order
     """
     if len(input_img.shape) == 4:
         input_img = np.expand_dims(
@@ -15,7 +16,7 @@ def apply_on_image(model, input_img, args):
 
     if not args.RuntimeAug:
         input_img = torch.from_numpy(input_img).float().cuda()
-        return model_inference(model, input_img, args)
+        return model_inference(model, input_img, args, squeeze, to_numpy)
     else:
         print("doing runtime augmentation")
 
@@ -27,7 +28,9 @@ def apply_on_image(model, input_img, args):
         input_img_aug_tensor = (
             torch.from_numpy(input_img_aug.astype(float)).float().cuda()
         )
-        out1 = model_inference(model, input_img_aug_tensor, args, squeeze=True)
+        out1 = model_inference(
+            model, input_img_aug_tensor, args, squeeze=True, to_numpy=True
+        )
 
         input_img_aug = []
         input_img_aug = input_img.copy()
@@ -38,7 +41,9 @@ def apply_on_image(model, input_img, args):
         input_img_aug_tensor = (
             torch.from_numpy(input_img_aug.astype(float)).float().cuda()
         )
-        out2 = model_inference(model, input_img_aug_tensor, args, squeeze=True)
+        out2 = model_inference(
+            model, input_img_aug_tensor, args, squeeze=True, to_numpy=True
+        )
 
         input_img_aug = []
         input_img_aug = input_img.copy()
@@ -49,10 +54,14 @@ def apply_on_image(model, input_img, args):
         input_img_aug_tensor = (
             torch.from_numpy(input_img_aug.astype(float)).float().cuda()
         )
-        out3 = model_inference(model, input_img_aug_tensor, args, squeeze=True)
+        out3 = model_inference(
+            model, input_img_aug_tensor, args, squeeze=True, to_numpy=True
+        )
 
         input_img_tensor = torch.from_numpy(input_img.astype(float)).float().cuda()
-        out0 = model_inference(model, input_img_tensor, args, squeeze=True)
+        out0 = model_inference(
+            model, input_img_tensor, args, squeeze=True, to_numpy=True
+        )
 
         for ch_idx in range(len(out0)):
             out0[ch_idx] = 0.25 * (
@@ -61,11 +70,10 @@ def apply_on_image(model, input_img, args):
                 + np.flip(out2[ch_idx], axis=1)
                 + np.flip(out3[ch_idx], axis=0)
             )
+        return np.expand_dims(out0, axis=0)  # add batch dimension
 
-        return out0
 
-
-def model_inference(model, input_img, args, squeeze=False):
+def model_inference(model, input_img, args, squeeze=False, to_numpy=False):
     with torch.no_grad():
         result = sliding_window_inference(
             inputs=input_img,
@@ -73,10 +81,12 @@ def model_inference(model, input_img, args, squeeze=False):
             sw_batch_size=1,
             predictor=model.forward,
             overlap=0.25,
-            mode="gaussian",
+            # mode="gaussian",
+            # sigma_scale=0.01,
         )
     if squeeze:
         result = torch.squeeze(result, dim=0)  # remove batch dimension
+    if to_numpy:
         result = result.cpu().numpy()
     return result
 